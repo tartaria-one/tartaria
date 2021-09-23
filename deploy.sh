ca81b65c58868e51478e597690655d7bb3c1627b
blackchestnut <alexander.kalinichev@gmail.com>
2021-09-23 22:53:01 +0300

    Update repo in deploy script

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ git init
 git add -A
 git commit -m 'deploy'
 
-git push -f git@github.com:blackchestnut/tartaria.git master:gh-pages
+git push -f git@github.com:tartaria-one/tartaria.git master:gh-pages
 
 cd -
